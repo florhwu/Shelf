7a1c437b8e6ed10fc5349b65422528e6156516c2
oaklen <florhwu@gmail.com>
2016-04-26 09:58:05 +0800

    db connected

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ var uri = 'mongodb://heroku_9fpb00cc:e3fghoh83m0mko4sf8acj318aa@ds061318.mlab.co
 //port: 61318
 //db name: heroku_9fpb00cc
 
+var seedData = [{
+    name: 'Atlas Shrugged',
+    author: 'Ayn Rand',
+    genre: 'fic'
+}];
+
 mongodb.MongoClient.connect(uri, function(err, db) {
   
     if(err) throw err;
@@ -32,62 +38,70 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 
     var bookList = db.collection('bookList');
 
-    //get
-    app.get('/bookList', function(req, res) {
-        console.log("SEREVR: get request received")
+    // Note that the insert method can take either an array or a dict.
 
-        //find all books in db
-        bookList.find(function(err,docs) {
-            console.log(docs);
-            //send data badk to controller
-            res.json(docs);
+    bookList.insert(seedData, function(err, result) {
+
+        if(err) throw err;  
+
+        //get
+        app.get('/bookList', function(req, res) {
+            console.log("SEREVR: get request received")
+
+            //find all books in db
+            bookList.find(function(err,docs) {
+                console.log(docs);
+                //send data badk to controller
+                res.json(docs);
+            });
+
         });
 
-    });
 
-    //post
-    app.post('/bookList', function(req, res) {
-        //print data recived from command prompt
-        console.log(req.body);
-        req.body._id= 0;
-        bookList.insert(req.body, function(err, doc) {
-            //send back data to controller
-            res.json(doc);
-        });
-    });
-
-    //delete
-    app.delete('/bookList/:id', function(req, res) {
-        var id = req.params.id;
-        console.log(id);
-
-        bookList.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
-            res.json(doc);
-        });
-    });
-
-    //edit
-    app.get('/bookList/:id', function(req, res) {
-        var id = req.params.id;
-        console.log(id);
-
-        bookList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
-            res.json(doc);
+        //post
+        app.post('/bookList', function(req, res) {
+            //print data recived from command prompt
+            console.log(req.body);
+            req.body._id= 0;
+            bookList.insert(req.body, function(err, doc) {
+                //send back data to controller
+                res.json(doc);
+            });
         });
 
-    });
+        //delete
+        app.delete('/bookList/:id', function(req, res) {
+            var id = req.params.id;
+            console.log(id);
 
-    //update
-    app.put('/bookList/:id', function (req, res) {
-      var id = req.params.id;
-      console.log("id is: " + id);
-      console.log("req body name: " + req.body.name);
-      bookList.findAndModify({
-        query: {_id: mongojs.ObjectId(id)},
-        update: {$set: {name: req.body.name, author: req.body.author, genre: req.body.genre}},
-        new: true}, function (err, doc) {
-          res.json(doc);
-        })
+            bookList.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+                res.json(doc);
+            });
+        });
+
+        //edit
+        app.get('/bookList/:id', function(req, res) {
+            var id = req.params.id;
+            console.log(id);
+
+            bookList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+                res.json(doc);
+            });
+
+        });
+
+        //update
+        app.put('/bookList/:id', function (req, res) {
+          var id = req.params.id;
+          console.log("id is: " + id);
+          console.log("req body name: " + req.body.name);
+          bookList.findAndModify({
+            query: {_id: mongojs.ObjectId(id)},
+            update: {$set: {name: req.body.name, author: req.body.author, genre: req.body.genre}},
+            new: true}, function (err, doc) {
+              res.json(doc);
+            })
+        });
     });
 });
 
