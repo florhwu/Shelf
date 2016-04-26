d88712285e3da656985e3e8d27875cb36d523623
oaklen <florhwu@gmail.com>
2016-04-26 09:08:57 +0800

    server mongo fix

--- server.js
+++ server.js
@@ -1,103 +1,116 @@
 
 var express = require('express');
 var mongojs = require('mongojs');
-// var favicon = require('serve-favicon');
 // var db = mongojs('bookList',['bookList']);
 var bodyParser = require('body-parser');
-// var mongodb = require('mongodb');
 var http = require('http'); 
 var url = require('url');
-// var client = require('mongodb').MongoClient;
-// var mongoose = require('mongoose');
+var mongodb = require('mongodb');
 
 var app = express();
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.
 
-// var dbConnUrl = process.env.MONGOLAB_URI ||
-//     'mongodb://127.0.0.1:3000/bookList'
-
-console.log('db server: ' + dbConnUrl)
+var seedData = [
+  {
+    name: 'Atlas Shrugged',
+    author: 'Ayn Rand',
+    genre: 'Fic'
+  },
+  {
+    name: 'Painter of Battles',
+    author: 'Olivia Newton-John',
+    genre: 'Art'
+  }
+];
 
 //where to look for static files
 app.use(express.static(__dirname +'/public'));
-//parse json data
 app.use(bodyParser.json());
 
-// client.connect(dbConnUrl, {}, function(err, db){
-
-//     console.log('error: ' + err)
-var MongoClient = require('mongodb').MongoClsient
-  , assert = require('assert');
-
 // Connection URL
-var url = 'mongodb://127.0.0.1:64501/shelves';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+var uri = 'mongodb://heroku_9fpb00cc:e3fghoh83m0mko4sf8acj318aa@ds061318.mlab.com:61318/heroku_9fpb00cc';
+//db user: heroku_9fpb00cc
+//db passworld: e3fghoh83m0mko4sf8acj318aa
+//host: ds061318.mlab.com
+//port: 61318
+//db name: heroku_9fpb00cc
 
-  db.close();
+mongodb.MongoClient.connect(uri, function(err, db) {
+  
+    if(err) throw err;
+
+    /*
+    * First we'll add a few songs. Nothing is required to create the 
+    * songs collection; it is created automatically when we insert.
+    */
+
+    var bookList = db.collection('bookList');
+
+    // Note that the insert method can take either an array or a dict.
+
+    bookList.insert(seedData, function(err, result) {
+
+        if(err) throw err;
+
+        //get
+        bookList.get('/bookList', function(req, res) {
+            console.log("SEREVR: get request received")
+
+            //find all books in db
+            db.bookList.find(function(err,docs) {
+                console.log(docs);
+                //send data badk to controller
+                res.json(docs);
+            });
+
+        });
+
+
+        //post
+        bookList.post('/bookList', function(req, res) {
+            //print data recived from command prompt
+            console.log(req.body);
+            req.body._id= 0;
+            db.bookList.insert(req.body, function(err, doc) {
+                //send back data to controller
+                res.json(doc);
+            });
+        });
+
+        //delete
+        bookList.delete('/bookList/:id', function(req, res) {
+            var id = req.params.id;
+            console.log(id);
+
+            db.bookList.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+                res.json(doc);
+            });
+        });
+
+        //edit
+        bookList.get('/bookList/:id', function(req, res) {
+            var id = req.params.id;
+            console.log(id);
+
+            db.bookList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+                res.json(doc);
+            });
+
+        });
+
+        //update
+        bookList.put('/bookList/:id', function (req, res) {
+          var id = req.params.id;
+          console.log("id is: " + id);
+          console.log("req body name: " + req.body.name);
+          db.bookList.findAndModify({
+            query: {_id: mongojs.ObjectId(id)},
+            update: {$set: {name: req.body.name, author: req.body.author, genre: req.body.genre}},
+            new: true}, function (err, doc) {
+              res.json(doc);
+            }
+          );
+        });    
+
+
+    });
 });
-    //get
-    app.get('/bookList', function(req, res) {
-        console.log("SEREVR: get request received")
-
-        //find all books in db
-        db.bookList.find(function(err,docs) {
-            console.log(docs);
-            //send data badk to controller
-            res.json(docs);
-        });
-
-    });
-
-
-    //post
-    app.post('/bookList', function(req, res) {
-        //print data recived from command prompt
-        console.log(req.body);
-        req.body._id= 0;
-        db.bookList.insert(req.body, function(err, doc) {
-            //send back data to controller
-            res.json(doc);
-        });
-    });
-
-    //delete
-    app.delete('/bookList/:id', function(req, res) {
-        var id = req.params.id;
-        console.log(id);
-
-        db.bookList.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
-            res.json(doc);
-        });
-    });
-
-    //edit
-    app.get('/bookList/:id', function(req, res) {
-        var id = req.params.id;
-        console.log(id);
-
-        db.bookList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
-            res.json(doc);
-        });
-
-    });
-
-    //update
-    app.put('/bookList/:id', function (req, res) {
-      var id = req.params.id;
-      console.log("id is: " + id);
-      console.log("req body name: " + req.body.name);
-      db.bookList.findAndModify({
-        query: {_id: mongojs.ObjectId(id)},
-        update: {$set: {name: req.body.name, author: req.body.author, genre: req.body.genre}},
-        new: true}, function (err, doc) {
-          res.json(doc);
-        }
-      );
-    });
-// });
-app.listen(3000);
-console.log("SERVER RUNNING ON PORT 3000");
